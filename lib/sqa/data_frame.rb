3d5193b21203a2aaf698d68e662d0f3d25782979
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-10-02 16:59:34 -0500

    snapshot

--- lib/sqa/data_frame.rb
+++ lib/sqa/data_frame.rb
@@ -4,8 +4,8 @@
 # TODO: Consider replacing Daru::Dataframe with
 #       rover which is used by prophet.
 
-# require_relative  'data_frame/yahoo_finance'
-# require_relative  'data_frame/alpha_vantage'
+require_relative  'data_frame/yahoo_finance'
+require_relative  'data_frame/alpha_vantage'
 
 # class Daru::DataFrame
 
@@ -38,18 +38,35 @@ class SQA::DataFrame < Rover::DataFrame
 
   #################################################
   def self.load(ticker, type=:csv, options={}, &block)
-    source  = SQA.data_dir + "#{ticker}.#{type}"
+    if ticker.is_a?(Pathname)
+      source  = ticker
+      type    = ticker.extname.to_s[1..].to_sym
+    else
+      source  = SQA.data_dir + "#{ticker}.#{type}"
+    end
 
     if :csv == type
-     from_csv(source, options={}, &block)
+     from_csv(source, options={})
     elsif :json == type
       from_json(source, options={}, &block)
     elsif %i[txt dat].include?(type)
       from_plaintext(source, options={}, &block)
     elsif :xls == type
       from_excel(source, options={}, &block)
+    elsif :parquet == type
+      from_parquet(source, options)
     else
       raise SQA::BadParameterError, "un-supported file type: #{type}"
     end
+  end
+
+
+  def self.from_csv(source, options={})
+    Rover.read_csv(source, options)
+  end
+
+
+  def self.from_parquet(source, options={})
+    Rover.read_parquet(source, options)
   end
 end
